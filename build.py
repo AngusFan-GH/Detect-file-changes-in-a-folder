e5895897242c7cf001ffb15e17c63bfed253d316
fanyingdong <fanyd@ccccit.com.cn>
2024-03-06 17:18:59 +0800

    ✨ feat: 打包时询问是否开启console面板

--- build.py
+++ build.py
@@ -4,19 +4,25 @@ import subprocess
 # 要打包的Python脚本的名称
 script_name = input("请输入要打包的Python脚本的名称（包括扩展名）：")
 
-# 检查输入名相同的旧文件是否存在，如果存在则删除
-if os.path.exists('dist/' + script_name):
-    os.remove('dist/' + script_name)
+# 询问是否需要console窗口
+need_console = input("是否需要console窗口？（y/n）")
 
 # PyInstaller的命令参数
 # --onefile 创建单个可执行文件
 # --noconsole 不显示命令行窗口（适用于GUI应用）
 # --icon=app.ico 添加图标，如果需要的话，路径需替换为实际图标的路径
 # 如果不需要图标，可以从命令中移除 --icon 参数
-pyinstaller_command = f'pyinstaller --onefile --noconsole {script_name}'
+if need_console.lower() == 'n':
+    command = f'pyinstaller --onefile {script_name}'
+else:
+    command = f'pyinstaller --onefile --noconsole {script_name}'
+
+# 检查输入名相同的旧文件是否存在，如果存在则删除
+if os.path.exists('dist/' + script_name):
+    os.remove('dist/' + script_name)
 
 # 执行命令
-process = subprocess.run(pyinstaller_command, shell=True)
+process = subprocess.run(command, shell=True)
 
 # 检查命令执行结果
 if process.returncode == 0:
